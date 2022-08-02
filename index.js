4f8d2eac108fceece0770c7ee616ceccdd51778f
suman <ronaldosuman97@gmail.com>
2022-08-02 11:57:14 +0530

    add: setname func

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
 import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
+import inquirer from "inquirer";
 import { board } from "./utils/board.js";
-
+let playerName;
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 const rainbow = chalkAnimation.rainbow("Welcome to Tic-Tac-Toe"); // Animation starts
 await intro();
+await setName();
 
 async function intro() {
-  await sleep(1000);
-  // board({ i: chalk.cyan("O"), b: "x", e: chalk.yellow("X") });
-  // ${chalk.bgHex("#d43e02")("How to play ?")}
-
+  await sleep(500);
   rainbow.stop();
+
   console.log(`
 ${chalk.bgBlue("How to play?")}
  Keep in mind your sign is ${chalk.cyanBright("O")}
  and my sign is ${chalk.magentaBright("X")} if you
  lose you'll get ${chalk.bgRed("killed")}.
 `);
-
-  await sleep(1500);
+  await sleep(750);
 
   console.log(`${chalk.bgHex("#d43e02")("hold")} on ...`);
-  await sleep(850);
+  await sleep(400);
   console.log(`
 ${await board({
-  a: "a",
-  b: "b",
-  c: "c",
-  d: "d",
-  e: "e",
-  f: "f",
-  g: "g",
-  h: "h",
-  i: "i",
+  a: "1",
+  b: "2",
+  c: "3",
+  d: "4",
+  e: "5",
+  f: "6",
+  g: "7",
+  h: "8",
+  i: "9",
 })}
 ☠️ keep these positions in ${chalk.bgMagenta.bold("mind")} ☠️
 `);
 }
+
+async function setName() {
+  const input = await inquirer.prompt({
+    name: "player_name",
+    message: "what's your name",
+  });
+
+  playerName = input.player_name;
+}
+
+console.log(`Hello ${playerName}`);
+
+async function main() {}
